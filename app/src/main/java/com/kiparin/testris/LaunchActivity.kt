8504207d19ba036b6489f79e4c7a7ebc6802ee26
Кипарин Алексей <kiparin@elma-bpm.com>
2024-01-13 15:47:46 +0300

    Start screen

---
+++ app/src/main/java/com/kiparin/testris/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package com.kiparin.testris
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
